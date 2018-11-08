4a469398854e93e372c3ed8bb6225ef4cb68ad77
Naeem <nbudhwa@uwo.ca>
2018-11-08 15:16:01 -0500

    Retrieve info for guest account

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,10 @@
 #include "Account.h"
 
 using namespace std;
-/*
+private:
+
+string accountType;
+
 bool Login()
 {
 	string password;
@@ -11,19 +14,38 @@ bool Login()
 	Account account = Account (password);
 	string type = account.GetAccountType();
 	
+	if (type == GUEST){
+		accountType = "GUEST";
+	}
+	else if (type == PATIENT){
+		accountType = "PATIENT";
+	}
+
 	type == PATIENT || type == GUEST ? true : false;
 }
-*/
 
 int main() 
 {
-	/*
 	if(!Login())
 	{
 		cout << "Log in failed" << endl;
 	}
-	*/
 	
+	if (accountType.equals("GUEST")) {
+		cout << "What would you like to access?";
+		cout << "1 - Medical History";
+		cout << "2 - Glucose Level";
+		string option;
+		getline(cin, option);
+		if (option.equals("1")) {
+			patient.getRecordEntries();
+		}
+		else {
+			
+		}
+	}
+
+
 	// prompt user to input patient data
 	Patient patient = new Patient();
 
