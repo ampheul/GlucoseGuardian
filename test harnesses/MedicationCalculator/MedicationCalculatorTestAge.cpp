90bdcc1da844e43538e3dded75be26747361c5a9
jdong56 <jdong56@uwo.ca>
2018-11-08 22:22:53 -0500

    Age and Glucose Test

--- test harnesses/MedicationCalculator/MedicationCalculatorTestAge.cpp
+++ test harnesses/MedicationCalculator/MedicationCalculatorTestAge.cpp
@@ -16,17 +16,21 @@ using namespace std;
 
 void TestAge()
 {	
-	double prePubescent = 6;
+	double prePubescent = 7;
 	double postPubescent = 40;
+
+	string sleepTime = "12:12:12";
+    struct tm tm;
+    strptime(sleepTime.c_str(), "%H:%M:%S", &tm);
 	
-	MedicationCalculator calcPre (39, 100, prePubescent, tm, "Low", 0, "Basal");
-	MedicationCalculator calcPost (39, 100, postPubescent, tm, "Low", 0, "Basal");
+	MedicationCalculator calcPre (8, 100, prePubescent, tm, "None", 10, "Bolus");
+	MedicationCalculator calcPost (8, 100, postPubescent, tm, "None", 10, "Bolus");
 	
 	HormoneDose preDosage = calcPre.computeDosage();
 	HormoneDose postDosage = calcPost.computeDosage();
 	
-	int preHormone = preDosage.getHormoneAmount();
-	int postHormone = postDosage.getHormoneAmount();
+	double preHormone = preDosage.getHormoneAmount();
+	double postHormone = postDosage.getHormoneAmount();
 		
 	if (preHormone == postHormone)
 	{
