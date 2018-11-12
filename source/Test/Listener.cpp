860939ad1c99f109ffdea278178e15d02c287688
Graeme Brabers <gbraber@uwo.ca>
2018-11-12 11:44:16 -0500

    Updating Laptop Output

--- source/Test/Listener.cpp
+++ source/Test/Listener.cpp
@@ -1,36 +1,56 @@
-#include <netinet/in.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
-#define SERVER_BUFFER_SIZE      1024
-
-int main()
-{
-    int socketId = socket(PF_INET, SOCK_STREAM, 0);
-
-    struct sockaddr_in serverAddr;
-    bzero((char*)&serverAddr, sizeof(serverAddr));
-    serverAddr.sin_family       = AF_INET;
-    serverAddr.sin_port         = htons(3307);
-    serverAddr.sin_addr.s_addr  = INADDR_ANY;
-    bind(socketId, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-    listen(socketId, 5);
-
-    while(true)
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+#define PORT 3307 
+int main(int argc, char const *argv[]) 
+{ 
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+    while(1)
     {
-        struct  sockaddr_storage    serverStorage;
-        socklen_t                   addr_size   = sizeof serverStorage;
-        int newSocket = accept(socketId, (struct sockaddr*)&serverStorage, &addr_size);
-
-        char        buffer[SERVER_BUFFER_SIZE];
-        int         get = read(newSocket, buffer, SERVER_BUFFER_SIZE - 1);
-
-        buffer[get] = '\0';
-        fprintf(stdout, "%s\n", buffer);
+        valread = read( new_socket , buffer, 1024); 
+        printf("%s\n",buffer ); 
     }
-    close(socketId);
-}
+    return 0; 
+} 
