1c2ecfdc76ae747268db6e2228ac0f7c544868c5
Thomas Vandeven <tvandev4@uwo.ca>
2018-11-15 17:48:13 -0500

    renamed 'test harnesses'->'tests' to remove evil space.

---
+++ tests/Account/AccountTestHarnesses.cpp
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string>
+#include <sstream>
+
+#include "Account.h"
+
+using namespace std;
+
+int main()
+{
+	string password;
+	cout << "Please enter a password: " << endl;
+	getline(cin, password);
+
+	Account account = Account(password);
+
+	return 0;
+}
+
+bool PatientTestPass_CorrectPassword()
+{
+	const string password = "patient password";
+	Account account = Account(password);
+	string type = account.GetAccountType();
+	type == account.PATIENT ? true : false;
+};
+
+bool PatientTestFail_IncorrectPassword()
+{
+	const string password = "not the password";
+	Account account = Account(password);
+	string type = account.GetAccountType();
+	type == account.UNKNOWN ? true : false;
+}
+
+bool GuestTestPass_CorrectPassword()
+{
+	const string password = "guest password";
+	Account account = Account(password);
+	string type = account.GetAccountType();
+	type == account.GUEST ? true : false;
+};
+
+bool GuestTestFail_IncorrectPassword()
+{
+	const string password = "not the guest password";
+	Account account = Account(password);
+	string type = account.GetAccountType();
+	type == account.UNKNOWN ? true : false;
+}
