b16702e872f1097abfe9880b7ea3ac755f854caa
Thomas Vandeven <tvandev4@uwo.ca>
2018-11-26 23:42:46 -0500

    adding to report.

--- source/Report/ReportMaker.cpp
+++ source/Report/ReportMaker.cpp
@@ -10,30 +10,57 @@ void ReportMaker::makeReport()
 {
     /**
      * Method:
-     * pipe latex code into a latex file and use this to pipe into latex.
-     * The file we pipe to will be included as input in the latex header
+     * Dynamically alter contents for use in latex by adding 
      * */
-    int toLatex[2];
-    pipe(toLatex);
-    FILE* latexWrite = fdopen(toLatex[1],"w");
+       
+    std::string commandDefinitions =  latexNewCommand("patient", "") 
+        + latexNewCommand("weight", std::to_string(patient.getWeight()))
+        + latexNewCommand("height", std::to_string(patient.getHeight()))
+        + latexNewCommand("age", std::to_string(patient.getAge())
+        + latexNewCommand("sex", patient.getSex()));
 
-    fprintf(latexWrite, "\\documentclass{article}");
-
+    std::string options = 
+        "-jobname " + std::to_string(std::time(NULL))
+        + "-output-directory " + "build/output/latex";
     
-
-
+    system("pdflatex " + options + " \"" + commandDefinitions+"\"");
+    
     return;
 }
-/** return a string for a latex command.
+
+/** Returns a string for a latex command.
  * 
  * Returns a string for a latex command in the form 
  *      \newcommand{\[command]}{\[definition]}
  * where [command] and [definition] are replaced with the respective arguments.
  * */
+
 std::string latexNewCommand(std::string command, std::string definition)
 {
-    return "\\newcommand{\\" + command + "}[\\" + defintion + "}"
+    return "\\newcommand{\\" + command + "}[\\" + definition + "}";
 }
 
+std::string latexHeader()
+{
+    return "\\documentclass{article}\n";
+}
+
+std::string latexBegin(std::string environment)
+{
+    return "\\begin{" + environment + "}";
+}
+
+std::string latexEnd(std::string environment)
+{
+    return "\\end{" + environment + "}";
+}
+
+std::string inEnvironment(std::string environment, std::string text)
+{
+    return 
+        latexBegin(environment) + "\n"
+        + text + "\n"
+        + latexEnd(environment);
+}
 
     
