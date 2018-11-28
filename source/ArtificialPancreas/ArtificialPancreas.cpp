95179d3c212d6df893648031673b08c32b3f105c
VeronicaWitzig <vwitzig@uwo.ca>
2018-11-27 20:08:52 -0500

    fixing account.cpp

--- source/ArtificialPancreas/ArtificialPancreas.cpp
+++ source/ArtificialPancreas/ArtificialPancreas.cpp
@@ -1,5 +1,14 @@
+/*!
+	@author Graeme Brabers
+	Responsible for managing patient info and determining insulin administration for the patient
+*/
+
 #include "ArtificialPancreas.h"
 
+/*!
+	@name ArtificialPancreas
+	@purpose constructor
+*/
 ArtificialPancreas::ArtificialPancreas()
 {
     user = new PatientInfo();
@@ -7,22 +16,41 @@ ArtificialPancreas::ArtificialPancreas()
     email = new EmailNotification(user);
 }
 
+/*!
+	@name ~ArtificialPancreas
+	@purpose destructor
+*/
 ArtificialPancreas::~ArtificialPancreas()
 {
     delete user;
     delete output;
 }
 
+/*!
+	@name setPatient
+	@purpose sets user
+	@param *newPatient - patient info to set
+*/
 void ArtificialPancreas::setPatient(PatientInfo *newPatient)
 {
     user = newPatient();
 }
 
+/*!
+	@name getPatientInfo
+	@purpose gets the patient info
+	@return the patient info
+*/
 PatientInfo ArtificialPancreas::getPatientInfo()
 {
     return user;
 }
 
+/*!
+	@name calculateMedication
+	@purpose calculates the medication needed for the patient based on their glucose reading
+	@param reading - the glucose reading
+*/
 void ArtificialPancreas::calculateMedication(const double reading)
 {
     if(reading > 1.7 && reading < 40)
@@ -45,6 +73,11 @@ void ArtificialPancreas::calculateMedication(const double reading)
     }
 }
 
+/*!
+	@name manuallyAdministerInsulin
+	@purpose administers the amount of insulin to the patient
+	@param insulinAmount - the insulin amount to administer
+*/
 void ArtificialPancreas::manuallyAdministerInsulin(const double insulinAmount)
 {
 	HormoneDose dose = new HormoneDose(BOLUS_INSULIN, insulinAmount);
