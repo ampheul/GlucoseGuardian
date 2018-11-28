8c707f1e2f8fad2f54dcabbfa9a250d750878e80
VeronicaWitzig <vwitzig@uwo.ca>
2018-11-28 14:23:56 -0500

    doxygen comments for email notification h/cpp

--- headerFiles/EmailNotification.h
+++ headerFiles/EmailNotification.h
@@ -1,6 +1,6 @@
-/*
-	@author Veronica Witzig
-	Purpose: Header file for the EmailNotification class
+/**
+*	\author Veronica Witzig
+*	Header file for the EmailNotification class
 */
 
 #ifndef EMAIL_NOTIFICATION_HEADER
@@ -17,24 +17,48 @@
 
 class EmailNotification
 {
-public:	
+public:
+	/// constructor, expects a pointer to PatientInfo
 	EmailNotification(PatientInfo *patientInfo);
+	
+	/// destructor
 	~EmailNotification();
+	
+	/// notifies the patient when unauthorized user attempts to login
 	void sendUnauthorizedAccessEmail();
+	
+	/// notifies the patient when a hypoglycemic event is imminent
 	void sendHypoglycemicEventEmail();
+	
+	/// notifies the emergency contact that a hypoglycemic event has occured
 	void sendEmergencyContactEmail();
+	
+	/// sends patient medical records
 	void sendMedicalRequestEmail();
 
 private:
+	/// email template used for unauthorized access attempt
 	const std::string UNAUTHORIZED_ACCESS_EMAIL_TEMPLATE = "unauthorized_email.txt";		//! email template for an unauthorized access
+	
+	/// email template used for a hypoglycemic event
 	const std::string HYPOGLYCEMIC_EVENT_EMAIL_TEMPLATE = "hypoglycemic_email.txt";			//! email template for a hypoglycemic event
+	
+	/// email template used for notifying the emergency contact
 	const std::string EMERGENCY_CONTACT_EMAIL_TEMPLATE = "emergency_email.txt";				//! email template for sending to an emergency contact
+	
+	/// email template used for sending medical records
 	const std::string REQUEST_MEDICAL_RECORDS_EMAIL_TEMPLATE = "request_records_email.txt";	//! email template for when a request for the patient's records has been made
 
+	/// contains the email of the sender (the patient's email)
 	std::string senderEmail;
+	
+	/// contains the password of the sender's email
 	std::string emailPassword;
+	
+	/// contains the email of the recipient (the emergency contact's email)
 	std::string recipientEmail;
 
+	/// sends an email using the specified email template
 	void sendEmail(std::string senderEmail, std::string recipientEmail, std::string emailPassword, std::string emailTemplate);
 };
 
--- source/Notification/EmailNotification.cpp
+++ source/Notification/EmailNotification.cpp
@@ -1,15 +1,15 @@
-/*!
-	@author Veronica Witzig
-	Purpose: Responsible for sending email notifications to gmail accounts
+/**
+*	\author Veronica Witzig
+*	Responsible for sending email notifications to gmail accounts
 */
 
 #include "EmailNotification.h"
 
 using namespace std;
 
-/*!
-	Name: EmailNotification
-	Description: constructor
+/**
+*	\name EmailNotification
+*	constructor
 */
 EmailNotification::EmailNotification(PatientInfo *patientInfo)
 {
@@ -19,63 +19,65 @@ EmailNotification::EmailNotification(PatientInfo *patientInfo)
 	recipientEmail = emergContact->getEmail();
 };
 
-/*!
-	Name: ~EmailNotification
-	Description: destructor
+/**
+*	\name ~EmailNotification
+*	destructor
 */
 EmailNotification::~EmailNotification() {};
 
-/*!
-	Name: sendHypoglycemicEventEmail
-	Description: sends an email from the patient's email to themself to warn the patient that a hypoglycemic event may occur,
-				 assumes the sender has a gmail account
+/**
+*	\name sendHypoglycemicEventEmail
+*	sends an email from the patient's email to themself to warn the patient that a hypoglycemic event may occur,
+*	assumes the sender has a gmail account
 */
 void EmailNotification::sendHypoglycemicEventEmail()
 {
 	EmailNotification::sendEmail(senderEmail, senderEmail, emailPassword, UNAUTHORIZED_ACCESS_EMAIL_TEMPLATE);
 };
 
-/*!
-	Name: sendUnauthorizedAccessEmail
-	Description: sends an email from the patient's email to themself to warn the patient that an unauthorized user has 
-				 attempted to log into their account, assumes the sender has a gmail account
+/**
+*	\name sendUnauthorizedAccessEmail
+*	sends an email from the patient's email to themself to warn the patient that an unauthorized user has 
+*	attempted to log into their account, assumes the sender has a gmail account
 */
 void EmailNotification::sendUnauthorizedAccessEmail()
 {
 	EmailNotification::sendEmail(senderEmail, recipientEmail, emailPassword, HYPOGLYCEMIC_EVENT_EMAIL_TEMPLATE);
 };
 
-/*!
-	Name: sendEmergencyContactEmail
-	Description: sends an email from the patient's email to their emergency contact to notify the guardiam that a 
-				 hypoglycemic event has occured, assumes the sender has a gmail account
+/**
+*	\name sendEmergencyContactEmail
+*	sends an email from the patient's email to their emergency contact to notify the guardiam that a 
+*	hypoglycemic event has occured, assumes the sender has a gmail account
 */
 void EmailNotification::sendEmergencyContactEmail()
 {
 	EmailNotification::sendEmail(senderEmail, recipientEmail, emailPassword, EMERGENCY_CONTACT_EMAIL_TEMPLATE);
 };
 
-/*!
-	Name: sendEmergencyContactEmail
-	Description: sends an email from the patient's email to their own email to notify them that a guardiam has
-				 requested to see their medical records
+/**
+*	\name sendEmergencyContactEmail
+*	sends an email from the patient's email to their own email to notify them that a guardiam has
+*	requested to see their medical records
 */
 void EmailNotification::sendMedicalRequestEmail()
 {
 	EmailNotification::sendEmail(senderEmail, recipientEmail, emailPassword, REQUEST_MEDICAL_RECORDS_EMAIL_TEMPLATE);
 };
 
-/*!
-	Name: SendEmail
-	Description: sends an email from the sender's address to the recipient's address, assumes the sender has a gmail account
-	@param string senderEmail - sender's email address
-	@param string recipientEmail - recipient's email address
-	@param string emailPassword - password for the email account
+/**
+*	\name SendEmail
+*	sends an email from the sender's address to the recipient's address, assumes the sender has a gmail account
+*	\param string senderEmail - sender's email address
+*	\param string recipientEmail - recipient's email address
+*	\param string emailPassword - password for the email account
 */
 void EmailNotification::sendEmail(string senderEmail, string recipientEmail, string emailPassword, string emailTemplate)
 {
 	cout << "Sending email to: " << recipientEmail << endl;
 	string command = "curl --url \'smtps://smtp.gmail.com:465\' --ssl-reqd --mail-from \'" + senderEmail + "\' --mail-rcpt \'" + recipientEmail + "\' --upload-file" + emailTemplate + "--user \'" + senderEmail + ":" + emailPassword + "\'";
+	
+	/// system() expects a const char[]*
 	const char* commandToChar = command.c_str();
 	system(commandToChar);
 	cout << "Email sent" << endl;
