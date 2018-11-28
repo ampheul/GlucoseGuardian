c90313664c2a3c7b2936e61df3954d519a8e2c01
Graeme Brabers <gbraber@uwo.ca>
2018-11-27 21:14:14 -0500

    flow test

--- tests/PathTest.cpp
+++ tests/PathTest.cpp
@@ -7,6 +7,6 @@ int main()
     ArtificialPancreas *artificialPancreas = new ArtificialPancreas();
 	GlucoseMonitor *glucoseMonitor = new GlucoseMonitor();
 
-	std::thread t1(glucoseMonitor->listen(artificialPancreas));
+	std::thread t1(glucoseMonitor->listen, artificialPancreas);
 	t1.join();
 }
