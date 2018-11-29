f42e67363774df37bde2044eed4520574918bedd
Graeme Brabers <gbraber@uwo.ca>
2018-11-29 16:09:34 -0500

    working on file io

--- tests/PatientFileTest.cpp
+++ tests/PatientFileTest.cpp
@@ -1,10 +1,13 @@
 #include "PatientInfo.h"
+#include "FileManager.h"
 
 int main()
 {
     PatientInfo *p = new PatientInfo();
+    FileManager *fm = new FileManager();
     p->getMonitorRecords()->push_back(MonitorRecord(std::time(NULL), GlucoseReading(15.0)));
     p->getMedicationRecords()->push_back(MedicationRecord(std::time(NULL), HormoneDose(BASAL_INSULIN, 5)));
+    fm->writeToFile(p);
     delete p;
 
     return 0;
