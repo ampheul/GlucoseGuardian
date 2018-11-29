655e20d196427aea7da37988f6e28f14465ae1d9
Thomas Vandeven <tvandev4@uwo.ca>
2018-11-29 18:18:20 -0500

    commited stuff to pull

--- headerFiles/HormoneType.h
+++ headerFiles/HormoneType.h
@@ -2,9 +2,12 @@
 #define HORMONETYPE_H
 
 /// Types of insulin.
-enum class HormoneType { 
-	BASAL_INSULIN, 
+enum class HormoneType {
+	/// Long term acting insulin
+	BASAL_INSULIN,
+	/// Short term acting insulin for taking after meals
 	BOLUS_INSULIN, 
+	/// Hormone to raise glucose levels. Counteracts insulin during hypoglycemic emergencies.
 	GLUCAGON 
 };
 
