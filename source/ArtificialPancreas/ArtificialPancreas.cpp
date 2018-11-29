bbe68dc775eff7f4e95678756f7f2d93594c9e9e
Graeme Brabers <gbraber@uwo.ca>
2018-11-28 20:06:51 -0500

    debugging

--- source/ArtificialPancreas/ArtificialPancreas.cpp
+++ source/ArtificialPancreas/ArtificialPancreas.cpp
@@ -67,13 +67,13 @@ void ArtificialPancreas::calculateMedication(const double reading, std::string b
         delete dose;
     }
     delete calculator;
-
+	/*
 	for(vector<MonitorRecord>::iterator it = user->getMonitorRecords()->begin(); it != user->getMonitorRecords()->end(); ++it)
 	{
 		std::cout << it->getRecordTime() << std::endl;
 		std::cout << it->getReading().getAmount() << std::endl;
 	}
-
+	*/
 }
 
 /*!
