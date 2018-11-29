6205132d1b2c9599bb8a0fe9cddc30fbe0bdd9da
Graeme Brabers <gbraber@uwo.ca>
2018-11-29 17:30:03 -0500

    working on file io

--- tests/PatientFileTest.cpp
+++ tests/PatientFileTest.cpp
@@ -8,8 +8,8 @@ int main()
     if(fm->checkForPatientFile())
     {
         p = fm->readFromFile();
-        //p->getMonitorRecords()->push_back(MonitorRecord(std::time(NULL), GlucoseReading(17.0)));
-        //p->getMedicationRecords()->push_back(MedicationRecord(std::time(NULL), HormoneDose(BASAL_INSULIN, 17)));
+        p->getMonitorRecords()->push_back(MonitorRecord(std::time(NULL), GlucoseReading(17.0)));
+        p->getMedicationRecords()->push_back(MedicationRecord(std::time(NULL), HormoneDose(BASAL_INSULIN, 17)));
     }
     else
     {
